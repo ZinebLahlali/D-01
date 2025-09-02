b74621c9753f891602dec2f05c7469d29548d853
“ZinebLahlali” <zeynepturna1992@gmail.com>
2025-09-02 22:52:49 +0100

    conversion de temp

---
+++ Conversion de la température/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   float k,c;
+   printf ("entrer la temperature en Celsius: ");
+   scanf ("%f",&c);
+   k = c + 273.15;
+   printf (" la temperature en Kelvin est %.2f", k);
+
+    return 0;
+}
